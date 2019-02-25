b93b6fac2e67900ef4905605580852d41ff04ac7
0422hui <980399260@qq.com>
2019-02-25 05:19:49 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,11 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
+Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+//相当于以下
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+
