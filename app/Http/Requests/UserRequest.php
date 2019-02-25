5c4fb76cce76c5210b8c9e05493ee133439a363f
0422hui <980399260@qq.com>
2019-02-25 08:46:19 +0000

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -18,11 +18,14 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' =>'mimes:jpeg,bmp,png,jpg,gif|dimensions:min_width=208,min_height=208',
         ];
     }
     public function messages()
     {
-        return[
+         return[
+             'avatar.mines'=>'请上传：jpeg,bmp,png,jpg,gif类型的文件',
+             'avatar.dimensions'=>'请上传宽高超过208的图片',
             'name.required'=>'已经有人注册了这个用户名了哦，请换一个',
             'name.regex'=>'用户名只能支持英文、数字、横杠和下划线哦',
             'name.between'=>'用户名要介于 3 - 25 个字符之间。',
