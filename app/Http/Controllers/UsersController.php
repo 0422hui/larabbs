fe98f1248c282715789e71decee0588cd220cc93
0422hui <980399260@qq.com>
2019-02-25 07:01:11 +0000

    显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,6 +18,6 @@ class UsersController extends Controller
     public function update(UserRequest $request, User $user)
     {
         $user->update($request->all());
-        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+        return redirect()->route('users.show', compact('user'))->with('success', '个人资料更新成功！');
     }
 }
