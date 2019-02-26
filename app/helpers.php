359223ea4cd60653b4c7a20c71bba0d2786f0450
0422hui <980399260@qq.com>
2019-02-26 02:27:04 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,6 @@
     function route_class(){
         return str_replace('.','_',Route::currentRouteName());
     }
+    function category_nav_active($category_id){
+        return active_class((if_route('categories.show')&&if_route_param('category',$category_id)));
+    }
