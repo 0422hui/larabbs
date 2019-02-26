5f0faf9a651224e36c4d204fe5c97d79ff9a241a
0422hui <980399260@qq.com>
2019-02-26 01:26:31 +0000

    填充用户与话题

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,19 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    //随机生成小段落
+    $sentence=$faker->sentence();
+    //随机生成一个月内的时间---更新时间
+    $updated_at=$faker->dateTimeThisMonth();
+    //随机生成一个月以内的时间，但是不能超过更新时间，因为创建时间肯定比更新时间要早---创建时间
+    $created_at=$faker->dateTimeThisMonth($updated_at);
+
+
     return [
-        // 'name' => $faker->name,
+        'title'=>$sentence,
+        'body'=>$faker->text(),//随机生成大段落文本 --内容
+        'excerpt'=>$faker->text(),//摘录字段
+        'updated_at'=>$updated_at,
+        'created_at'=>$created_at,
     ];
 });
